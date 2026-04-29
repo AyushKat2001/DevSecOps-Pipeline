0918f3fd2f672013491dc41d9345962da1e522a8
AyushKat2001 <ayushkatiyar925@gmail.com>
2026-04-29 16:02:54 +0530

    Project pushed v1

--- src/app.js
+++ src/app.js
@@ -20,4 +20,12 @@ app.post('/api/echo', (req, res) => {
   res.status(200).json({ received: req.body });
 });
 
+const PORT = process.env.PORT || 3000;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
 module.exports = app;
